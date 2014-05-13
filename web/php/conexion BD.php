1920ac220d9ea5ca3f052d14f8ee119c824c38ac
CharlyCB <carlos.coloborde@gmail.com>
2014-05-13 19:27:32 +0200

    Comprobacion de ficheros y funciones

--- web/php/conexion BD.php
+++ web/php/conexion BD.php
@@ -1,5 +1,7 @@
 <?php
 
+	//FUNCIONA CORRECTAMENTE
+
 		$link = mysql_connect('localhost', 'user', 'contraseña') or die('No se pudo conectar: ' . mysql_error());
 				echo 'Connected successfully';
 				
@@ -16,11 +18,15 @@
 	
 <?php
 	//comprobar loggin
+	
+	//Posibilidad de hacer el loggin mediante esta forma, preguntar a chema si es posible esto:
+	
+	
 	$user = isset($_POST["usuario"]);
 	$contra = isset($_POST["contra"]);
 	
 	$query = "SELECT Pass FROM cliente WHERE correo='$user'";
-	$result = mysql_query($query) or die('Consulta fallida ' . mysql_error());
+	$result = mysql_query($query) or die('Usuario no encontrado ' . mysql_error());
 	if ($result == "" || $result == null || $result != $contra) {
 		echo "el usuario o la contraseña no son los correctos";
 	}
